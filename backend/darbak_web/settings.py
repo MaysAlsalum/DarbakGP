0b291f66ea772145e16a0d3338b5003576a70df9
MaysAlslom <mays2alsalloum@hotmail.com>
2026-02-16 20:07:02 +0300

    fixing the Arc (database)

--- backend/darbak_web/settings.py
+++ backend/darbak_web/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'darbak_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
 
